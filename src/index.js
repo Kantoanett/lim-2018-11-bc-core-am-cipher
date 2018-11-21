ae5c6b303325496be494b288b490028a05ff988b
Kantoanett <antoanett.rodriguez@gmail.com>
2018-11-21 13:13:53 -0500

    Primer paso cifrado

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
+//Creando una variable a través del id asigando al elemento
 let hiddenCode = document.getElementById("hidden-code");
 let mainText = document.getElementById("enter-text");
-//Creamos una nueva variable para guardar el nuevo texto, darle valor a la cadena vacía, y luego al momento de cifrar o descifrar aparezca en el input
-let newWord = "";
-const btnCipher = document.getElementById("btn-cipher")
-const btnDescipher = document.getElementById("btn-descipher")
+//Creando una nueva variable para guardar el nuevo texto, darle valor a la cadena vacía, y luego al momento de cifrar o descifrar aparezca en el input
+let newWord;
+//Creando una constante en la que se va obtener el elemento con id btnCipher y btnDescipher
+const btnCipher = document.getElementById("btn-cipher");
+const btnDescipher = document.getElementById("btn-descipher");
+const result = document.getElementById("resultado");
 
 //Función que se ejecuta al hacer click en Cifrar
-btnCipher.addEventListener("click", Cifrar);
+//btnCipher.addEventListener("click",encode);
 
-//Función que mostrará el texto al hacer click en cifrar
-function encode(hiddenCodeValue,mainTextValue){
-//Bucle para obtener el código ASCII de cada letra ingresada en el texto
-  for(let i=0; i<mainTextValue.length; i++){
-    let mainTextAscii = mainTextValue.charCodeAt(i);
-    
+//Codificar: Función que mostrará el texto al hacer click en cifrar
+function encode(){
+//Crear variable para guardar valor
+  let offset = parseInt(hiddenCode.value);
+  let string = mainText.value;
+//Poner valor en la caja de texto
+  result.value=string;
+//Asignando valor de cadena vacía
+  newWord = "";
+  for(let i=0; i<string.length; i++){
+//Variable para obtener el código ASCII de cada letra ingresada en el texto
+    let mainTextAscii = string.charCodeAt(i);
+      if (65 <= string.charCodeAt(i) <= 90) {
+/*Reemplazo la variable en la fórmula para obtener la letra original del alfabeto
+newWord = newWord + (String.fromCharCode ((((string.charCodeAt(i)-65) + offset) % 26) + 65));*/
+      newWord = newWord + (String.fromCharCode ((((mainTextAscii-65) + offset) % 26) + 65));
+      }
+}return (newWord);
 }
-}
+
+btnCipher.addEventListener("click",encode);
