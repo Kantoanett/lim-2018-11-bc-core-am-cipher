9d373fa5b69eaae0343d419523e74bc231c0b08e
Kantoanett <antoanett.rodriguez@gmail.com>
2018-11-26 08:33:00 -0500

    objeto-cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,41 @@
 window.cipher = {
-  // ... 
+encode: function(offset, string){
+  newWord = "";
+/*Bucle para obtener el código ASCII de cada letra ingresada en el texto*/
+  for(let i=0; i<string.length; i++){
+    let mainTextAscii = string.charCodeAt(i);
+/*Condicional para obtener el código ASCII de cada letra al darse el offset*/
+      if (65 <= string.charCodeAt(i) <= 90) {
+/*Mostrar la nueva palabra, concatenando letras*/
+      newWord = newWord + (String.fromCharCode ((((mainTextAscii-65) + offset) % 26) + 65));
+}
+/*Condicional para que se reconozca el espacio mediante su código ASCII, y luego sea transformado y se concatene como espacio*/
+      else if (mainTextAscii === 32) {
+      newWord = newWord + String.fromCharCode(mainTextAscii);
+}
+}
+//Con esto, devuelve el cifrado en la caja de texto
+  return result.value = newWord;
+},
+
+decode: function(offset, string){
+  newWord = "";
+/*Bucle para obtener el código ASCII de cada letra ingresada en el texto*/
+  for(let i=0; i<string.length; i++){
+    let mainTextAscii = string.charCodeAt(i);
+/*Condicional para obtener el código ASCII de cada letra al darse el offset*/
+      if (65 <= string.charCodeAt(i) <= 90) {
+/*Mostrar la nueva palabra, concatenando letras*/
+      newWord = newWord + (String.fromCharCode ((((mainTextAscii-90) - offset) % 26) + 90));
+}
+/*Condicional para que se reconozca el espacio mediante su código ASCII, y luego sea transformado y se concatene como espacio*/
+      else if (mainTextAscii === 32) {
+      newWord = newWord + String.fromCharCode(mainTextAscii);
+}
+}
+//Con esto, devuelve el cifrado en la caja de texto
+  return result.value = newWord;
+
+},
+
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-//Creando una variable a través del id asigando al elemento
+
+//Creando una variable para obtener el elemento colocado en el input
 let hiddenCode = document.getElementById("hidden-code");
 let mainText = document.getElementById("enter-text");
 //Creando una nueva variable para guardar el nuevo texto, darle valor a la cadena vacía, y luego al momento de cifrar o descifrar aparezca en el input
@@ -6,63 +7,40 @@ let newWord;
 //Creando una constante en la que se va obtener el elemento con id btnCipher y btnDescipher
 const btnCipher = document.getElementById("btn-cipher");
 const btnDescipher = document.getElementById("btn-descipher");
+//Creando una constante en la que se va a obtener el elemento con id resultado
 const result = document.getElementById("resultado");
 
 //Función que se ejecuta al hacer click en Cifrar
-btnCipher.addEventListener("click",encode);
-
+btnCipher.addEventListener("click",hiddenText);
 //Codificar: Función que mostrará el texto al hacer click en cifrar
-function encode(){
-//Crear variable para guardar valor
-  let offset = parseInt(hiddenCode.value);
-  let string = mainText.value.toUpperCase();
-/*Poner valor en la caja de texto
-  result.value=newWord;*/
-//Asignando valor de cadena vacía
-  newWord = "";
-  for(let i=0; i<string.length; i++){
-//Variable para obtener el código ASCII de cada letra ingresada en el texto
-    let mainTextAscii = string.charCodeAt(i);
-      if (65 <= string.charCodeAt(i) <= 90) {
-/*Reemplazo la variable en la fórmula para obtener la letra original del alfabeto
-newWord = newWord + (String.fromCharCode ((((string.charCodeAt(i)-65) + offset) % 26) + 65));*/
-      newWord = newWord + (String.fromCharCode ((((mainTextAscii-65) + offset) % 26) + 65));
-}
-      else if (mainTextAscii === 32) {
-      newWord = newWord + String.fromCharCode(mainTextAscii);
-}
-}
-}
+function hiddenText(){
+//Creando la variable para obtener elemento colocado en el input hidden-code y para guardar su valor
+  let hiddenCodeValue = parseInt(hiddenCode.value);
+//Creando la variable para obtener elemento colocado en el input main-text y para guardar su valor
+  let mainTextValue = mainText.value.toUpperCase();
+//Con esta variable se va a obtener el valor después de ejecutar encode:function dada la conexión con cipher.encode
+    newWord = cipher.encode(hiddenCodeValue, mainTextValue);
 //Con esto, devuelve el cifrado en la caja de texto
-} return result.value = newWord;
+  return result.value = newWord;
 }
 
 //Función que se ejecuta al hacer click en Descifrar
-btnDescipher.addEventListener("click",decode);
-
+btnDescipher.addEventListener("click",revealedText);
 //Decodificar: Función que mostrará el texto al hacer click en descifrar
-function decode(){
-  let offset = parseInt(hiddenCode.value);
-  let string = mainText.value.toUpperCase();
-  /*Poner valor en la caja de texto
-    result.value=newWord;*/
-  newWord = "";
-  for(let i=0; i<string.length; i++){
-//Variable para obtener el código ASCII de cada letra ingresada en el texto
-    let mainTextAscii = string.charCodeAt(i);
-      if (65 <= string.charCodeAt(i) <= 90) {
-/*Reemplazo la variable en la fórmula para obtener la letra original del alfabeto
-newWord = newWord + (String.fromCharCode ((((string.charCodeAt(i)-65) + offset) % 26) + 65));*/
-      newWord = newWord + (String.fromCharCode ((((mainTextAscii-90) - offset) % 26) + 90));
-}
-      else if (mainTextAscii === 32) {
-     newWord = newWord + String.fromCharCode(mainTextAscii);
-}
-}
-}
+function revealedText(){
+//Creando la variable para obtener elemento colocado en el input hidden-code y para guardar su valor
+  let hiddenCodeValue = parseInt(hiddenCode.value);
+//Creando la variable para obtener elemento colocado en el input main-text y para guardar su valor
+  let mainTextValue = mainText.value.toUpperCase();
+//Con esta variable se va a obtener el valor después de ejecutar decode:function dada la conexión con cipher.decode
+    newWord = cipher.decode(hiddenCodeValue, mainTextValue);
+
 //Con esto, devuelve el descifrado en la caja de texto
-} return result.value = newWord;
+  return result.value = newWord;
 }
-result.addEventListener("click", () => {
+
+//Agregamos al boton final, el evento click para que la pagina se reinicie
+document.getElementById("btn-reboot").addEventListener("click",() => {
   location.reload ();
+  /*result.addEventListener("click", () => {*/
 });
