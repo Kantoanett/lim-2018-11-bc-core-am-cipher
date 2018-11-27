56f621e8188df3a7e004f74b49c0c86b0e6aac96
Kantoanett <antoanett.rodriguez@gmail.com>
2018-11-27 13:21:38 -0500

    test-por-corregir-errores

--- src/cipher.js
+++ src/cipher.js
@@ -5,23 +5,21 @@ let  newWord = "";
 /*Bucle para obtener el código ASCII de cada letra ingresada en el texto*/
   for(let i=0; i<string.length; i++){
     let mainTextAscii = string.charCodeAt(i);
-/*Condicional para obtener el código ASCII de cada letra al darse el offset*/
+//Condicional para obtener el código ASCII de cada letra al darse el offset
       if (65 <= string.charCodeAt(i) <= 90) {
-/*Mostrar la nueva palabra, concatenando letras*/
+//Mostrar la nueva palabra, concatenando letras
       newWord = newWord + (String.fromCharCode ((((mainTextAscii-65) + offset) % 26) + 65));
-}
-/*Condicional para que se reconozca el espacio mediante su código ASCII, y luego sea transformado y se concatene como espacio*/
-      else if (mainTextAscii === 32) {
-      newWord = newWord + String.fromCharCode(mainTextAscii);
+      }
+      else if (mainTextAscii === 32){
+        newWord = newWord + String.fromCharCode (mainTextAscii);
 }
 }
 //Con esto, devuelve el cifrado en la caja de texto
-  let result;
-  return result.value = newWord;
+  return newWord;
 },
 
 decode: function(offset, string){
-let  newWord = "";
+let newWord = "";
 /*Bucle para obtener el código ASCII de cada letra ingresada en el texto*/
   for(let i=0; i<string.length; i++){
     let mainTextAscii = string.charCodeAt(i);
@@ -31,14 +29,12 @@ let  newWord = "";
       newWord = newWord + (String.fromCharCode ((((mainTextAscii-90) - offset) % 26) + 90));
 }
 /*Condicional para que se reconozca el espacio mediante su código ASCII, y luego sea transformado y se concatene como espacio*/
-      else if (mainTextAscii === 32) {
+      if (mainTextAscii === 32) {
       newWord = newWord + String.fromCharCode(mainTextAscii);
 }
 }
 //Con esto, devuelve el cifrado en la caja de texto
-   let result;
-   return result.value = newWord;
-
+  return newWord;
 },
 
 };
